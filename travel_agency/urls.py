ae369b8186571d3852499c1e55396c4313fdfaf2
abhishekdubey1412 <abhishekdubey@gmail.com>
2024-04-10 15:52:32 +0530

    url redirection

--- travel_agency/urls.py
+++ travel_agency/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,6 +13,8 @@ urlpatterns = [
     path('', include('blog.urls')),
     path('', include('about.urls')),
     path('', include('dashboard.urls')),
+    path('', RedirectView.as_view(url='http://www.boatwale.com/', permanent=True)),
+    path('', RedirectView.as_view(url='http://boatwale.com/', permanent=True)),
 ] 
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
