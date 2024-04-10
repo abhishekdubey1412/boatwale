f1520a8edcb5af2b871a86692ce14d71661599e4
abhishekdubey1412 <abhishekdubey@gmail.com>
2024-04-10 15:14:37 +0530

    added alt field

--- product/models.py
+++ product/models.py
@@ -10,6 +10,7 @@ class Boat(models.Model):
     capacity = models.IntegerField()
     description = models.TextField()
     image = models.ImageField(upload_to='boats/')
+    image_alt = models.CharField(max_length=100, null=True, blank=True)
 
     def __str__(self):
         return self.name
