3624afbd3675ea7f659fb0ced1d17df7286d168b
abhishekdubey1412 <abhishekdubey@gmail.com>
2024-04-11 23:42:32 +0530

    created a robots.txt file

--- travel_agency/urls.py
+++ travel_agency/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,6 +13,7 @@ urlpatterns = [
     path('', include('blog.urls')),
     path('', include('about.urls')),
     path('', include('dashboard.urls')),
+    path('robots.txt/', TemplateView.as_view(template_name="robots.txt", content_type='text/plain')),
 ] 
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
