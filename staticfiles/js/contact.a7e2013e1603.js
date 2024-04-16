5b2f4294df65a993a656ecf6a2a87e276de9c2f1
abhishekdubey1412 <abhishekdubey@gmail.com>
2024-04-16 15:24:35 +0530

    create staticfiles

--- staticfiles/js/contact.a7e2013e1603.js
+++
@@ -1,5 +0,0 @@
-var message_timeout = document.getElementById("message_alert");
-
-setTimeout(function () {
-    message_timeout.style.setProperty("display", "none");
-}, 5000);
--- staticfiles/js/header.af0b0d806a73.js
+++
@@ -1,9 +0,0 @@
-Array.from(document.getElementsByTagName('input')).forEach((e, i) => {
-    e.addEventListener('keyup', (el) => {
-        if (e.value.length > 0) {
-            document.getElementsByClassName('bi-caret-down-fill')[i].style.transform = "rotate(180deg)";
-        } else {
-            document.getElementsByClassName('bi-caret-down-fill')[i].style.transform = "rotate(0deg)";
-        }
-    });
-});
