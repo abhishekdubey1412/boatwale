5814a513f0035b95a7452b1df95ca3faa8a4c8e2
abhishekdubey1412 <abhishekdubey@gmail.com>
2024-04-19 16:59:16 +0530

    working on meta data

--- home/views.py
+++ home/views.py
@@ -32,7 +32,7 @@ def home(request):
         'products': products,
         'recent_posts': recent_posts,
         'routes': routes,
-        'thumbnail': 'https://boatwale.com/static/images/banner/boatwale_thumnail.png',
+        'thumbnail': '../static/images/banner/boatwale_thumnail.png/',
         'type': 'website',
         'index_type': 'index'
     }
