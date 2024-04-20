e39e876d04e8df4aad6833fe021ac1f1ba829e2d
abhishekdubey1412 <abhishekdubey@gmail.com>
2024-04-20 21:36:13 +0530

    fixed mysql connection

--- travel_agency/settings.py
+++ travel_agency/settings.py
@@ -83,10 +83,21 @@ WSGI_APPLICATION = 'travel_agency.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'boatwale',
+        'USER': 'boatwale',
+        'PASSWORD': 'Boatwale@5100',
+        'HOST': '43.205.100.220',
+        'PORT': '3306',
     }
 }
 
